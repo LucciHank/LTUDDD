cb710ff9ae7e66174c563b99417a4b68866eba0b
Lê Trung Kiên <vietnamer123@gmail.com>
2024-01-24 02:28:08 +0700

    commit

--- HelloFood/app/src/main/java/com/example/hellofood/Activity/IntroActivity.java
+++ HelloFood/app/src/main/java/com/example/hellofood/Activity/IntroActivity.java
@@ -2,15 +2,42 @@ package com.example.hellofood.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.hellofood.R;
+import com.example.hellofood.databinding.ActivityIntroBinding;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends BaseActivity {
+    ActivityIntroBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        binding=ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setVariable();
+        getWindow().setStatusBarColor(Color.parseColor("FFE485"));
+    }
+
+    private void setVariable() {
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
