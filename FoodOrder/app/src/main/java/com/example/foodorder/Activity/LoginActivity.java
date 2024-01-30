59f9b78ded21b72cd02e4e1a95f1b40f2a90daad
Lê Trung Kiên <vietnamer123@gmail.com>
2024-01-31 03:46:55 +0700

    commit
    commit

---
+++ FoodOrder/app/src/main/java/com/example/foodorder/Activity/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.foodorder.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.foodorder.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends BaseActivity {
+ActivityLoginBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setVariable();
+
+
+    }
+    private void setVariable(){
+        binding.newsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=binding.loginEmail.getText().toString();
+                String password=binding.loginPass.getText().toString();
+                if (!email.isEmpty()&&!password.isEmpty()){
+                    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, task -> {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Login is succesful",Toast.LENGTH_SHORT).show();
+                            Intent intent= new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }else {
+                            Toast.makeText(LoginActivity.this,"Authentication failed",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }else {
+                    Toast.makeText(LoginActivity.this,"Please fill your email and password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
